38b08f763e59614418e65e3ce992d775b4fb5bb5
mitaka1303 <mitaka1332@outlook.jp>
2023-04-21 17:57:23 +0900

    post_test

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express();
 
 app.use(express.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const cors = require('cors')
+
+const corsOption ={
+    origin: process.env.URL,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors())
 
 //環境変数のロード
 const dotenv = require('dotenv');
@@ -23,7 +35,10 @@ app.get("/", async(req,res)=>{
 })
 
 
-
+app.post('/getList', (req, res)=>{
+    console.log("test: getList");
+    res.send('test : getList')
+})
 
 
 app.listen(4000, function(){console.log("run server")
